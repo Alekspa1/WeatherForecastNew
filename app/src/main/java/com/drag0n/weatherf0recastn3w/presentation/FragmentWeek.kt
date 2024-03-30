836deebbcce6e3f7342b2b5791198afad898beb5
Alex <apereverzev47@gmail.com>
2024-03-30 11:03:44 +0400

    корутины

--- app/src/main/java/com/drag0n/weatherf0recastn3w/presentation/FragmentWeek.kt
+++ app/src/main/java/com/drag0n/weatherf0recastn3w/presentation/FragmentWeek.kt
@@ -2,6 +2,7 @@ package com.drag0n.weatherf0recastn3w.presentation
 
 import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.drag0n.weatherf0recastn3w.MainViewModel
 import com.drag0n.weatherf0recastn3w.adapter.DaysAdapter
+import com.drag0n.weatherf0recastn3w.data.WeatherWeek.Spisok
 import com.drag0n.weatherf0recastn3w.databinding.FragmentWeekBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FragmentWeek : Fragment() {
     private lateinit var binding: FragmentWeekBinding
@@ -30,10 +37,25 @@ class FragmentWeek : Fragment() {
 
         val rc = binding.rcDay
         rc.layoutManager = LinearLayoutManager(view.context)
-        model.liveDataWeek.observe(viewLifecycleOwner) {
-            adapter = DaysAdapter(it.list)
-            rc.adapter = adapter
-        } // Заполнение погоды на неделю
+        CoroutineScope(Dispatchers.Main).launch {
+                adapter = DaysAdapter(get())
+                rc.adapter = adapter
+                Log.d("MyLog", "End: ${get().size}")
+        }
+//        model.liveDataWeek.observe(viewLifecycleOwner) {
+//            adapter = DaysAdapter(it.list)
+//            rc.adapter = adapter
+//        } // Заполнение погоды на неделю
+    }
+   suspend fun get(): List<Spisok> = withContext(Dispatchers.IO){
+       val list = arrayListOf<Spisok>()
+       for (i in 0..10){
+           delay(5000)
+           list.add(Spisok("Счетчик $i", null,null,null,null,null))
+           Log.d("MyLog", "Add: ${list.size}")
+       }
+       return@withContext list
+
     }
 
     companion object {
