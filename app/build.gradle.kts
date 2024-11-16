7caa44786eb9d63fb187875d0410f4580e39e0a7
DRAGON <apereverzev47@gmail.com>
2024-11-17 00:32:49 +0300

    Убрал фон в списке погоды по часам, сделал отображение каждого третьего элемента списка

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,15 @@ plugins {
 
 android {
     namespace = "com.drag0n.weatherf0recastn3w"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.drag0n.weatherf0recastn3w"
         minSdk = 26
         targetSdk = 33
 
-        versionCode = 19
-        versionName = "8"
+        versionCode = 22
+        versionName = "9"
 
 
 
@@ -64,8 +64,8 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.6.1")
     kapt ("androidx.room:room-compiler:2.6.1")
     implementation ("androidx.room:room-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.6")
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
 
 
     implementation ("com.squareup.picasso:picasso:2.8")
@@ -76,15 +76,15 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0") // Retrofit
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // Json
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("com.google.android.gms:play-services-location:21.3.0")
 
 
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
